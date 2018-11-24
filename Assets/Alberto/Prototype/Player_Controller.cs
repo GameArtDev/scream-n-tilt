9d98e127524655629ca281ab3706568c87dcbe10
Alberto <alberto.paparella.88@gmail.com>
2018-11-24 18:44:16 +0100

    Build v.0.2 (ancora prototipo) - Adesso con input da Mic

--- Assets/Alberto/Prototype/Player_Controller.cs
+++ Assets/Alberto/Prototype/Player_Controller.cs
@@ -5,6 +5,8 @@ public class Player_Controller : MonoBehaviour {
     public float MaximumOffSet = 3;
     public float MovementSpeed = 0.1f;
 
+    public bool isVoiceControlled;
+
     [SerializeField]
     UI_CustomButton leftButton;
     [SerializeField]
@@ -18,10 +20,31 @@ public class Player_Controller : MonoBehaviour {
 
     public void Update()
     {
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-            MoveLeft();
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-            MoveRight();
+        if (!isVoiceControlled)
+        {
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                MoveLeft();
+            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                MoveRight();
+        }
+        else
+        {
+            Move(MicrophoneInput.MicLoudnessDb + 30);
+        }
+    }
+
+    public void Move(float _value)
+    {
+        if (_value < -100)
+            _value = -100;
+
+        transform.position += Vector3.right * MovementSpeed * _value / 20;
+
+        if (transform.position.x <= -MaximumOffSet)
+            transform.position = new Vector3(-MaximumOffSet, transform.position.y, transform.position.z);
+        else if (transform.position.x >= MaximumOffSet)
+            transform.position = new Vector3(MaximumOffSet, transform.position.y, transform.position.z);
+
     }
 
     public void MoveLeft()
